4b5137be5c9bda8396e3a9d64ab3fb188a9ef44e
Jacob Hill <hilljake93@gmail.com>
2022-06-24 11:07:27 -0500

    checkpoint 1

--- school_roster_app/models.py
+++ school_roster_app/models.py
@@ -1,64 +1,65 @@
-from django.db import models
+# import csv
+# import os
 
-class Person:
+# class Person:
 
-    def __init__(self, name, age, password, role):
-        self.name = name
-        self.age = age
-        self.password = password
-        self.role = role
+#     def __init__(self, name, age, password, role):
+#         self.name = name
+#         self.age = age
+#         self.password = password
+#         self.role = role
 
-    @classmethod
-    def load_all(cls):
-        my_path = os.path.abspath(os.path.dirname(__file__))
-        file_path = os.path.join(my_path, cls.DATA_FILE)
+#     @classmethod
+#     def load_all(cls):
+#         my_path = os.path.abspath(os.path.dirname(__file__))
+#         file_path = os.path.join(my_path, cls.DATA_FILE)
         
-        people = []
+#         people = []
         
-        with open(file_path) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                person = cls(**row)
-                people.append(person)
+#         with open(file_path) as csvfile:
+#             reader = csv.DictReader(csvfile)
+#             for row in reader:
+#                 person = cls(**row)
+#                 people.append(person)
 
-        return people
+#         return people
 
-class School:
-    def __init__(self, name):
-        self.name = name
-        self.staff = Staff.load_all()
-        self.students = Student.load_all()
+# class School:
+#     def __init__(self, name):
+#         self.name = name
+#         self.staff = Staff.load_all()
+#         self.students = Student.load_all()
 
-    def find_student_by_id(self, school_id):
-        for student in self.students:
-            if student.school_id == school_id:
-                return student
+#     def find_student_by_id(self, school_id):
+#         for student in self.students:
+#             if student.school_id == school_id:
+#                 return student
         
-        return None
+#         return None
 
-    def find_staff_by_id(self, employee_id):
-        for staff in self.staff:
-            if staff.employee_id == employee_id:
-                return staff
+#     def find_staff_by_id(self, employee_id):
+#         for staff in self.staff:
+#             if staff.employee_id == employee_id:
+#                 return staff
         
-        return None
+#         return None
 
-class Staff(Person):
-    DATA_FILE = "../data/staff.csv"
+# class Staff(Person):
+#     DATA_FILE = "../data/staff.csv"
 
-    def __init__(self, name, age, password, role, employee_id):
-        super().__init__(name, age, password, role)
-        self.employee_id = employee_id
+#     def __init__(self, name, age, password, role, employee_id):
+#         super().__init__(name, age, password, role)
+#         self.employee_id = employee_id
 
-    def __repr__(self):
-        return f"staff: {self.name}"
+#     def __repr__(self):
+#         return f"staff: {self.name}"
 
-class Student(Person):
-    DATA_FILE = "../data/students.csv"
+# class Student(Person):
+#     DATA_FILE = "../data/students.csv"
 
-    def __init__(self, name, age, password, role, school_id):
-        super().__init__(name, age, password, role)
-        self.school_id = school_id
+#     def __init__(self, name, age, password, role, school_id):
+#         super().__init__(name, age, password, role)
+#         self.school_id = school_id
 
-    def __str__(self):
-        return f"{self.name.upper()}\n--------------\nage: {self.age}\nid: {self.school_id}"
+#     def __str__(self):
+#         return f"{self.name.upper()}\n--------------\nage: {self.age}\nid: {self.school_id}"
--- school_roster_app/views.py
+++ school_roster_app/views.py
@@ -1,3 +1,28 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
+from .models import School # import our School class
 
-# Create your views here.
+my_school = School("Django School") # create a school instance
+
+
+def index(request):
+    my_data = { 
+        "school_name": my_school.name
+    }
+    return render(request, "pages/index.html", my_data)
+
+
+def list_staff(request):
+    return HTTPResponse('List Staff')
+
+
+def staff_detail(request, employee_id):
+    pass # implement
+
+
+def list_students(request):
+    pass # implement
+
+
+def student_detail(request, student_id):
+    pass # implement
